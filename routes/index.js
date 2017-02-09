0d29d1353330652d9e4b332b6c277efd2956f3b4
Yun <cheese10yun@gmail.com>
2017-02-10 02:56:42 +0900

    - 불필요한 코드 제거 - 카톡 입장 / 방나감 로그 추가 - 카톡 차단 로그 추가

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,9 @@ router.get('/keyboard', (req, res) => {
     }).send(JSON.stringify(menu));
 });
 
+// TODO content 값이 이상한 값이라면 ?.
+// TODO 식단은 하루동안은 바뀌지 않기 떄문에 레디스에다가 케쉬해서 가져다 쓰자.
 router.post('/message', (req, res) => {
-    
-    // TODO content 값이 이상한 값이라면 ?.
-    // TODO 식단은 하루동안은 바뀌지 않기 떄문에 레디스에다가 케쉬해서 가져다 쓰자.
-    
-    
     const _obj = {
         user_key: req.body.user_key,
         type: req.body.type,
@@ -36,7 +33,7 @@ router.post('/message', (req, res) => {
     Bot.choseMenu(_obj.content, (err, result) => {
         if (!err) {
             console.log(result.message.text);
-            
+
             res.set({
                 'content-type': 'application/json'
             }).send(JSON.stringify(result));
@@ -50,40 +47,34 @@ router.post('/message', (req, res) => {
 
 router.post('/friend', (req, res) => {
     const
-        user_key = req.body.user_key,
-        result = {
-            message: {text: '환영 합니다.'}
-        };
+        user_key = req.body.user_key;
     
+    console.log(`${user_key}님이 쳇팅방에 참가했습니다.`);
     
     res.set({
         'content-type': 'application/json'
-    }).send(JSON.stringify(result));
-    
-    
+    }).send(JSON.stringify({success:true}));
 });
 
 router.delete('/friend', (req, res) => {
     const
-        user_key = req.body.user_key,
-        result = {
-            message: {text: '테스트입니다.'}
-        };
+        user_key = req.body.user_key;
+    
+    console.log(`${user_key}님이 쳇팅방을 차단했습니다.`);
     
     res.set({
         'content-type': 'application/json'
-    }).send(JSON.stringify(result));
+    }).send(JSON.stringify({success:true}));
 });
 
 router.delete('/chat_room/:user_key', (req, res) => {
-    const user_key = req.params.user_key,
-        result = {
-            message: {text: '테스트입니다.'}
-        };
+    const user_key = req.params.user_key;
+    
+    console.log(`${user_key}님이 쳇팅방에서 나갔습니다.`);
     
     res.set({
         'content-type': 'application/json'
-    }).send(JSON.stringify(result));
+    }).send(JSON.stringify({success:true}));
 });
 
 
