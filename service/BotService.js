0207c3f45ce9a9ac358937d8e3fc120fcfee6f54
Yun <cheese10yun@gmail.com>
2017-02-11 01:05:37 +0900

    message 서비스 객체 구현 	- message type 구성 일반 / 포토  2개로 구성 	- bot 객체에서 해당 메세지에 맞는 타입을 결정해서 다시 돌려줌

--- service/BotService.js
+++ service/BotService.js
@@ -8,8 +8,6 @@ const Bot = {};
 const cheerio = require('cheerio');
 const message = require('../service/message');
 
-const msg_base = message.base;
-const msg_photo = message.photo;
 
 // TODO 리턴 메시지들은 관리할것인가 아래 처럼 ? 아니면 별도로 구성 ??
 Bot.choseMenu = (content, callback) => {
@@ -17,8 +15,7 @@ Bot.choseMenu = (content, callback) => {
     switch (content) {
         case '교내식단':
             Bot.diet((err, result) => {
-                msg_base.message.text = result;
-                callback(err, msg_base);
+                callback(err, result);
             });
             break;
         case '메뉴2':
@@ -50,7 +47,6 @@ Bot.diet = (callback) => {
                 diet +='----------저녁---------\r\n';
                 diet += $(this).find("td").eq(2).text();
             });
-            
             
             callback(null, message.base(diet));
             
