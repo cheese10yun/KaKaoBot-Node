bb7639a2475d847918e85dd4f2144d3e023027bc
Yun <cheese10yun@gmail.com>
2017-02-16 22:31:46 +0900

    commit

--- service/message.js
+++ service/message.js
@@ -27,38 +27,38 @@ message.baseType = (text) => {
 
 message.photoType = (text, url_photo, label, url_button) => {
     return {
-        message: {
-            text: text,
-            photo: {
-                url: url_photo,
-                width: 640,
-                height: 480
-            },
-            message_button: {
-                label: label,
-                url: url_button,
-            }
+      message: {
+        text: text,
+        photo: {
+          url: url_photo,
+          width: 640,
+          height: 480
         },
-        keyboard: {
-            type: 'buttons',
-            buttons: message.buttons
+        message_button: {
+          label: label,
+          url: url_button,
         }
+      },
+      keyboard: {
+        type: 'buttons',
+        buttons: message.buttons
+      }
     }
 };
 
 message.messageButtonType = (text, label, url_button) => {
     return {
-        message: {
-            text: text,
-            message_button: {
-                label: label,
-                url: url_button,
-            }
-        },
-        keyboard: {
-            type: 'buttons',
-            buttons: message.buttons
+      message: {
+        text: text,
+        message_button: {
+          label: label,
+          url: url_button,
         }
+      },
+      keyboard: {
+        type: 'buttons',
+        buttons: message.buttons
+      }
     }
 };
 
